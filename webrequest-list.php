9a6d6f12781ab561f8a05afea6de1fef74a013d7
Francesco Bianco <info.francescobianco@gmail.com>
2022-01-24 13:24:39 +0100

    Update webrequest-list.php

--- webrequest-list.php
+++ webrequest-list.php
@@ -11,5 +11,5 @@ $response = $client->request('GET', 'https://api.github.com/users/'.$user.'/repo
 $repositories = json_decode($response->getBody()->getContents(), true);
 
 foreach ($repositories as $repository) {
-  echo $repository['name'].PHP_EOL;
+  echo $repository['full_name'].PHP_EOL;
 }
