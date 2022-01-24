671297fa0c5e7e4aa951077c764505c8fa1ed553
Francesco Bianco <info.francescobianco@gmail.com>
2022-01-24 14:44:52 +0100

    Update webrequest-list.php

--- webrequest-list.php
+++ webrequest-list.php
@@ -6,7 +6,9 @@ $client = new Client();
 
 $user = $_GET['user'] ?? 'javanile';
 
-$response = $client->request('GET', 'https://api.github.com/users/'.$user.'/repos');
+$url = 'https://api.github.com/users/'.$user.'/repos?per_page=100';
+
+$response = $client->request('GET', $url);
 
 $repositories = json_decode($response->getBody()->getContents(), true);
 
